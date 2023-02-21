a140bb93c07871bf0cc749190baf19148763671d
daniel.chan <daniel-daimiao-chen@outlook.com>
2023-02-21 18:01:32 +0800

    gitsigns: add plugin gitsigns and open line blame

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,12 +5,12 @@
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'nvim-lua/penary.nvim'
 
   use {
     'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup()
-    end
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function() require('gitsigns').setup{current_line_blame = true,} end
   }
 
   end)
